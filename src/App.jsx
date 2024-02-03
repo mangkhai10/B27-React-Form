6e4935bb74ce66f9a9047339c47a5f7384f693d4
Khai <muankhai10@gmail.com>
2024-02-03 17:53:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import "./style.css";
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,63 @@
+// SignUpForm.jsx
+import React, { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    // Form validation logic
+    if (username.length < 8) {
+        setError("Username must be at least 8 characters long.");
+        return;
+      }
+
+    try {
+      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const result = await response.json();
+      console.log("API Response: ", result);
+
+      // Assuming the API response has a 'token' property
+      const token = result.token;
+      // Do something with the token, e.g., store it in local storage for future use
+
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
